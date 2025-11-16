a5e562473a2d9b206569a0e0e099aaf366fe9635
grahmr <apedrialva@ymail.com>
2025-11-15 20:39:58 -0600

    Add LegacySkinProvider class for skin management

---
+++ osu.Eclipse.Game/Skins/LegacySkinProvider.cs
@@ -0,0 +1,13 @@
+namespace osu.Eclipse.Game.Skins
+{
+    public class LegacySkinProvider : ISkinProvider
+    {
+        public string Name => "LegacySkin";
+
+        public void ApplySkin()
+        {
+            // TODO: Parse and apply .osk (osu!stable) skin resources
+            // Implement fully featured legacy skin parsing and application here
+        }
+    }
+}
